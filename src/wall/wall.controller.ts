516a639ba8f78c93fb29f45743184c7059634f71
“Carlos <praimus666@gmail.com>
2023-07-03 13:18:57 -0500

    Fixed agora-messages endpoint

--- src/wall/wall.controller.ts
+++ src/wall/wall.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { WallService } from './wall.service';
+import { AgoraMessage } from './dto/agoraMessage.dto';
 
 @Controller('wall')
 export class WallController {
   constructor(private readonly wallService: WallService) {}
 
   @Get('agora-messages')
-  listAgoraMessages(@Query() email: string) {
-    return this.wallService.listAgoraMessages(email);
+  listAgoraMessages(@Query() agoraMessage: AgoraMessage) {
+    return this.wallService.listAgoraMessages(agoraMessage);
   }
 }
--- src/wall/wall.service.ts
+++ src/wall/wall.service.ts
@@ -4,20 +4,22 @@ import { InjectClient } from 'nest-mysql';
 import { RowDataPacket } from 'mysql2';
 import { Connection } from 'mysql2/promise';
 
+import { AgoraMessage } from './dto/agoraMessage.dto';
+
 @Injectable()
 export class WallService {
   constructor(
     @InjectClient('MySQL') private connection: Connection,
   ){}
 
-  async listAgoraMessages(email: string) {
-      const questions = await this.connection.query<RowDataPacket[]>(`
+  async listAgoraMessages(agoraMessage: AgoraMessage) {
+    const messages = await this.connection.query<RowDataPacket[]>(`
       SELECT h.index, h.title, h.body, h.link, h.effdt FROM ag_home_agora h
       WHERE h.effdt = (SELECT MAX(h_ed.effdt) FROM ag_home_agora h_ed WHERE h.index=h_ed.index AND h_ed.effdt <= current_timestamp)
       AND h.index NOT IN (SELECT nh.index FROM ag_home_agora_closed nh WHERE h.index=nh.index AND email=?)
       ORDER BY h.effdt
-      `, [email]);
+    `, [agoraMessage.email]);
 
-      return questions[0];
+    return messages[0];
   }
 }
